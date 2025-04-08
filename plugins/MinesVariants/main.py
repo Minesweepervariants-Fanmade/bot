d6677ef4080f3e77ff08de39fc7cfab08cf93bda
s_rh <S@H>
2025-04-08 15:49:57 +0800

    temp

--- plugins/MinesVariants/main.py
+++ plugins/MinesVariants/main.py
@@ -1,10 +1,10 @@
-import asyncio
 from pathlib import Path
-
 import yaml
-from ncatbot.plugin import BasePlugin, CompatibleEnrollment
-from ncatbot.core.message import GroupMessage, PrivateMessage
-from ncatbot.utils import logger
+
+if __name__ == "__import__":
+    from ncatbot.plugin import BasePlugin, CompatibleEnrollment
+    from ncatbot.core.message import GroupMessage
+    from ncatbot.utils import logger
 
 try:
     import ollama
@@ -22,21 +22,50 @@ download_json_path = f"{SELF_PATH}\\cache\\download.json"
 config_data = yaml.full_load(open(f"{SELF_PATH}/data.yaml", "r", encoding="utf-8"))
 
 
+async def rule_create(descri: str, post):
+    def post_ollama(message=None, content="", assistant="") -> ChatResponse:
+        if message is None:
+            message = [
+                {"role": "assistant", "content": assistant},
+                {"role": "user", "content": content}
+            ]
+        return chat(model=config_data["model"], messages=message)
+    await post("")
+    response: ChatResponse = post_ollama(
+        assistant="\n".join(config_data["params"]["check"]),
+        content=descri
+    )
+    print(response.message.content)
+    data = eval(response.message.content.rsplit("\"", 2)[1])
+    print("rule check:", data)
+    if not data:
+        await post("未描述规则,请检查并重新键入")
+        return
+    else:
+        await post("已通过规则检查")
+    response: ChatResponse = post_ollama(
+        assistant="\n".join(config_data["params"]["init_examp"]),
+        content=descri
+    )
+    print(response.message.content)
+    data = eval(response.message.content.rsplit("\"", 2)[1])
+    print("data:", data)
+    if not data:
+        await post("请给出规则的示例内容")
+        return
+    else:
+        await post("已获取到示例信息")
+
+
+
 class MinesVariants(BasePlugin):
     name = "MinesVariants"
     version = "0.0.0"
 
-    @staticmethod
-    def post_ollama(message=None, assistant="", descri="") -> ChatResponse:
-        if message is None:
-            message = [
-                {"role": "assistant", "content": assistant},
-                {"role": "user", "content": descri}
-            ]
-        return chat(model=config_data["model"], messages=message)
-
     @bot.group_event()
     async def on_group_event(self, msg: GroupMessage):
+        async def post(content):
+            await self.api.post_group_msg(msg.group_id, content)
         if msg.raw_message.startswith("#mv"):
             command = msg.raw_message.split(" ")[1:]
             if command[0] == "新建规则":
@@ -44,37 +73,15 @@ class MinesVariants(BasePlugin):
                     if message["type"] != "text":
                         await self.api.post_group_msg(msg.group_id, "无法识别非文字信息,请改用文字描述")
                         return
-                await self.api.post_group_msg(msg.group_id, "开始尝试理解规则")
-                descri = msg.raw_message.split(" ", 2)[2]
-                response = self.post_ollama(
-                    assistant="\n".join(config_data["params"]["check"]),
-                    descri=descri
-                )
-                print(response.message.content)
-                data = eval(response.message.content.rsplit("\"", 2)[1])
-                print("rule check:", data)
-                if not data:
-                    await self.api.post_group_msg(msg.group_id, "未描述规则,请检查并重新键入")
-                    return
-                else:
-                    await self.api.post_group_msg(msg.group_id, "已通过规则检查")
-                response: ChatResponse = self.post_ollama(
-                    assistant="\n".join(config_data["params"]["init_examp"]),
-                    descri=descri
-                )
-                print(response.message.content)
-                data = eval(response.message.content.rsplit("\"", 2)[1])
-                print("data:", data)
-                if not data:
-                    await self.api.post_group_msg(msg.group_id, "请给出规则的示例内容")
-                    return
-                else:
-                    await self.api.post_group_msg(msg.group_id, "已获取到示例信息")
-
+                return rule_create(descri=command[1], post=post)
 
 def main():
     # ollama.chat
     print(config_data)
+    descri = """
+    123
+    """
+    rule_create(descri, print)
 
 
 if __name__ == '__main__':
